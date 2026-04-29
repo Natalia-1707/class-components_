4e6c13913e31c5ef4feeee194e131a55a93a2e52
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-04-29 22:44:52 +0300

    feat: make css styles for SearchSection and ResultsSection

--- class-components-app/src/components/Results/Results.tsx
+++ class-components-app/src/components/Results/Results.tsx
@@ -5,11 +5,14 @@ class ResultsSection extends React.Component {
   render() {
     return (
         <section className="results-section">
-            <div className="results-header">
-                <div>Item Name</div>
-                <div>Item Description</div>
+            <h2>Results</h2>
+            <div className="results-wrapper">
+                <div className="results-header">
+                    <div>Item Name</div>
+                    <div>Item Description</div>
+                </div>
+                <div className="results-list"></div>
             </div>
-            <div className="results-list"></div>
             <button>Error</button>
         </section>
     )

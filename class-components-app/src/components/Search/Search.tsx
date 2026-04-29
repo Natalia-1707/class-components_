f05b52dbad1b412996fb86481f777eceae22c4fe
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-04-29 23:20:56 +0300

    feat: make search functionality with local storage

--- class-components-app/src/components/Search/Search.tsx
+++ class-components-app/src/components/Search/Search.tsx
@@ -1,13 +1,35 @@
 import "./search.css";
 import React from 'react';
 
-class SearchSection extends React.Component {
+type State = {
+  searchValue: string;
+};
+
+class SearchSection extends React.Component <{}, State> {
+    state: State = {
+    searchValue: '',
+  };
+
+  componentDidMount() {
+  const savedValue = localStorage.getItem('search');
+
+  if (savedValue) {
+    this.setState({ searchValue: savedValue });
+    }
+  }
+
+   handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    this.setState({ searchValue: value });
+    localStorage.setItem("search", value);
+  };
+
   render() {
     return (
       <section className="search-section">
           <form>
               <label htmlFor="search"></label>
-              <input type="text" id="search"className="input"/>
+              <input type="text" id="search"className="input" value={this.state.searchValue} onChange={this.handleChange}/>
           </form>
           <button>Search</button>
       </section>

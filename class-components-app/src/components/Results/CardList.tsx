1c9d42708d5c79184b3f7f40e0349528199dfd3d
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-05-01 12:24:52 +0300

    feat: make search results display

---
+++ class-components-app/src/components/Results/CardList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import type { Item } from "./types";
+
+type Props = {
+  items: Item[];
+};
+
+class CardList extends React.Component<Props> {
+  render() {
+    const { items } = this.props;
+
+    return (
+      <div className="results-list">
+        {items.map((item) => (
+          <div key={item.id} className="result-item">
+            <div>{item.name}</div>
+            <div>{item.description}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
--- class-components-app/src/components/Results/Results.tsx
+++ class-components-app/src/components/Results/Results.tsx
@@ -1,7 +1,37 @@
 import "./results.css";
 import React from 'react';
+import CardList from "./CardList";
+import type { Item } from "./types";
 
-class ResultsSection extends React.Component {
+type ResultsState = {
+  items: Item[];
+};
+
+class ResultsSection extends React.Component<{}, ResultsState> {
+  state: ResultsState = {
+    items: [
+      {
+        id: "1",
+        name: "Jean-Luc Picard",
+        description: "Captain of the USS Enterprise",
+      },
+      {
+        id: "2",
+        name: "Spock",
+        description: "Science officer, half-Vulcan",
+      },
+      {
+        id: "3",
+        name: "James T. Kirk",
+        description: "Captain of the USS Enterprise, famous for bold and risky decisions",
+      },
+      {
+        id: "4",
+        name: "Data",
+        description: "Android officer aboard the USS Enterprise-D, стремится понять человечность",
+      },
+    ],
+  };
   render() {
     return (
         <section className="results-section">
@@ -11,7 +41,7 @@ class ResultsSection extends React.Component {
                     <div>Item Name</div>
                     <div>Item Description</div>
                 </div>
-                <div className="results-list"></div>
+                <CardList items={this.state.items}/>
             </div>
             <button>Error</button>
         </section>

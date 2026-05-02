9282cd9f983e321ffe386473b5a9cc086ae95d92
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-05-02 20:19:09 +0300

    feat: make initial data load

---
+++ class-components-app/src/api/types.ts
@@ -0,0 +1,24 @@
+export type Item = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+export type RequestParams = {
+  pageNumber: string;
+  pageSize: string;
+  name?: string;
+};
+
+export type Character = {
+  uid: string;
+  name: string;
+  gender?: string;
+  yearOfBirth?: number;
+  yearOfDeath?: number;
+  placeOfBirth?: string;
+};
+
+export type CharactersResponse = {
+  characters?: Character[];
+};

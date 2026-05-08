55ebbf9c06691a6cb53d6701be4f6b7c55995d01
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-05-09 00:35:52 +0300

    test: add App tests

---
+++ src/App.error.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { test, expect, vi } from 'vitest';
+
+vi.mock('./components/Results/Results', () => ({
+  default: () => {
+    throw new Error('Test error');
+  },
+}));
+
+import App from './App';
+
+//ErrorBoundary//
+
+test('shows fallback UI when child throws error', () => {
+  render(<App />);
+  expect(screen.getByText(/something went wrong/i)).toBeInTheDocument();
+});
---
+++ src/App.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { test, expect, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+const fetchCharactersMock = vi.fn();
+
+vi.mock('./components/Results/Results', () => ({
+  default: React.forwardRef((_, ref) => {
+    React.useImperativeHandle(ref, () => ({
+      fetchCharacters: fetchCharactersMock,
+    }));
+
+    return <div data-testid="results" />;
+  }),
+}));
+
+import App from './App';
+
+// render App title//
+
+test('renders app title', () => {
+  render(<App />);
+  expect(screen.getByText(/Star Trek Search/i)).toBeInTheDocument();
+});
+
+//Verify props are passed correctly and handleSearch calls fetchCharacters//
+
+test('calls fetchCharacters when search is triggered', async () => {
+  const user = userEvent.setup();
+
+  render(<App />);
+
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button');
+
+  await user.type(input, 'spock');
+  await user.click(button);
+
+  expect(fetchCharactersMock).toHaveBeenCalledWith('spock');
+});
+

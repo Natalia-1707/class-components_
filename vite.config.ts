278f060b2ae94719cd1ed73f2ff3d16471152262
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-05-08 23:18:24 +0300

    test: configure vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/class-components_/",
   plugins: [react()],
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+
+    coverage: {
+      provider: 'v8',
+
+      reporter: ['text', 'html'],
+
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+
+      include: ['src/**/*.{ts,tsx}'],
+
+      exclude: [
+        'src/**/*.test.{ts,tsx}',
+        'src/**/*.spec.{ts,tsx}',
+        'src/main.tsx',
+        'src/setupTests.ts',
+        'src/**/*.d.ts',
+      ],
+    },
+  },
 });

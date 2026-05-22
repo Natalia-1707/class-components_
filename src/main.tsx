9edc71439e2a149ac0fff665e5048cf96e59f9e0
natalia-1707 <nataliatsepoukhowa@gmail.com>
2026-05-22 22:46:32 +0300

    feat: Redux Toolkit installed and store configured

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/class-components_">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/class-components_">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
